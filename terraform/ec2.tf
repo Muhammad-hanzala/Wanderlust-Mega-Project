7de82e9927416f1796981535e955f1216f76c5df
Muhammad Hanzala <103304142+Muhammad-hanzala@users.noreply.github.com>
2025-05-15 05:06:11 +0500

    Update ec2.tf
    open port 8080

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -130,6 +130,17 @@ resource "aws_security_group" "allow_user_to_connect" {
     security_groups  = []    
     self             = false 
   }
+  ingress { 
+    description = "port 8080 allow"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []    
+    prefix_list_ids  = []    
+    security_groups  = []    
+    self             = false 
+  }
   
   tags = {
     Name = "mysecurity"
